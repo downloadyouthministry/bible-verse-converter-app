4de095501a23b6109ac1f1aa95eda2d05f7bbb12
Steve Stone <steve@downloadyouthministry.com>
2020-05-21 15:33:24 -0400

    Updates

---
+++ precache-manifest.3ebb1872ead41f740e8a8b2121be818d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c5305da40ff057cfef7218ed2e69b9",
+    "url": "/bible-verse-converter-app/index.html"
+  },
+  {
+    "revision": "0e1877402ece77ba35b1",
+    "url": "/bible-verse-converter-app/static/css/main.79a42c1c.chunk.css"
+  },
+  {
+    "revision": "1140a3382e246ced19fe",
+    "url": "/bible-verse-converter-app/static/js/2.53198230.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bible-verse-converter-app/static/js/2.53198230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1877402ece77ba35b1",
+    "url": "/bible-verse-converter-app/static/js/main.bd348421.chunk.js"
+  },
+  {
+    "revision": "27de4102270010c6cace",
+    "url": "/bible-verse-converter-app/static/js/runtime-main.fce76b53.js"
+  }
+]);
